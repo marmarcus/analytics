41a6d03a392d728edd2269cb20a18d2de9b3cdb5
Marcus <marfae321@gmail.com>
2026-03-11 00:51:49 -0700

    Created Gemini prompt and output to JSON

--- gemini.py
+++ gemini.py
@@ -7,22 +7,87 @@ import pandas as pd
 import os
 import json
 
-# Get API key from .env
-#load_dotenv()
+#Get API key from .env
+load_dotenv()
 
-#api_key = os.getenv("GEMINI_API_KEY")
-#client = genai.Client(api_key=api_key)
+api_key = os.getenv("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
 
+# Function which contains the prompt for Gemini to analyze
+def analyze_description(posts, id_number):
+    prompt = f"""
+             Each description contains and ID followed by its description text.
+ 
+             Analyze the description and extract structured information that is applicable to each specified field.
+             Do not add the same item to multiple fields.
+
+             The output will be put in a JSON file so ONLY return a JSON array with a object matching each field and NOTHING else in the SAME ORDER as the descriptions.
+             The output must be formatted for JSON.
+
+             Programming languages include things like Python, SQL, R, Java, C++, etc.
+
+             Tools include software platforms such as Tableau, Power BI, Excel, AWS, Docker, Airflow, Kubernetes, etc.
+
+             If a field cannot be determined, return N/A into the field.
+
+             JSON Fields:
+             - ID
+             - years_experience 
+             - programming_languages
+             - tools
+
+             The output must ONLY be formatted for JSON.
+
+             Id:
+             {id_number}
+
+             {id_number}'s Description:
+             {posts}
+
+             """
+    
+    response = client.models.generate_content(
+        model='gemini-3.1-flash-lite-preview',
+        contents=(prompt)
+    )
+
+    return json.loads(response.text)
+    
 # Read CSV file 
 df = pd.read_csv('data/jobs.csv')
-descriptions = df['description'] # All description cells
-window = 5 # Amount of cells the AI will go through at a time 
 
+# Get all description column cells
+descriptions = df['description']
+id_number = df['id']
+# Amount of cells the AI will go through at a time 
+window_size = 1 
 
+output_file = "job_data.json"
 
-#response = client.models.generate_content(
-#    model="gemini-3.1-flash-lite-preview", 
-#    contents="Explain how AI works in a few words"
-#)
+# Check for output file, create if doesn't exist
+if not os.path.exists(output_file):
+    with open(output_file, "w") as file:
+        json.dump([], file)
 
-#print(response.text)
+# Loop through all the cells from 0 to end of descriptions array
+# Increment by the window size
+for i in range(0, 3, window_size):
+    
+    posts = descriptions[i:i + window_size]
+
+    results = analyze_description(posts, id_number)
+
+    # Start outputting results to JSON file
+
+    # Get the data from the JSON file
+    with open(output_file, "r") as file:
+        data = json.load(file)
+    
+    # Append results to the end of the JSON file
+    data.extend(results)
+    print(i)
+    print(success)
+
+    # Write to JSON file
+    with open(output_file, "w") as file:
+        json.dump(data, file, indent=4)
