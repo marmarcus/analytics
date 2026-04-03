021cab5bc074af6f67866715c914608d001b1200
Marcus <marfae321@gmail.com>
2026-04-03 01:07:21 -0700

    Ajusted Gemini prompt, added job postings

--- gemini.py
+++ gemini.py
@@ -9,9 +9,9 @@ import json
 import time
 
 # Function which contains the prompt for Gemini to analyze
-def analyze_description(posts, id_number):
+def analyze_description(posts, id_number, location, date):
     prompt = f"""
-             Each description contains and ID followed by its description text.
+             Each job description contains and ID followed by its description text.
  
              Analyze the description and extract structured information that is applicable to each specified field.
              Do not add the same item to multiple fields.
@@ -19,9 +19,13 @@ def analyze_description(posts, id_number):
              The output will be put in a JSON file so ONLY return a JSON array with a object matching each field and NOTHING else in the SAME ORDER as the descriptions.
              The output must be formatted for JSON.
 
+             Do not use parentheses for any reason whether its to give an acronym or to group tools or programming languages for example, Microsoft Office (Word, Excel, Powerpoint) should instead list either "Microsoft Office" OR "Word", "Excel", "Powerpoint".
+
              Programming languages include things like Python, SQL, R, Java, C++, etc.
 
              Tools include software platforms such as Tableau, Power BI, Excel, AWS, Docker, Airflow, Kubernetes, etc.
+
+             If listing out multiple keys for the fields programming_languages and tools, each item must be its own key. For example this is incorrect: "tools": "Claude, Stable Diffusion". It has to be "tools": ["Claude", "Stable Diffusion"]
 
              If a requirement is being currently enrolled then it is an internship.
              
@@ -29,7 +33,11 @@ def analyze_description(posts, id_number):
 
              JSON Fields:
              - ID
-             - years_experience 
+             - location
+             - date
+             - years_experience
+             - salary
+             - in-person/remote/hybrid
              - fulltime/parttime/internship
              - programming_languages
              - tools
@@ -38,6 +46,12 @@ def analyze_description(posts, id_number):
 
              Id:
              {id_number}
+
+             Location:
+             {location}
+
+             Date:
+             {date}
 
              {id_number}'s Description:
              {posts}
@@ -61,9 +75,11 @@ df = pd.read_csv('job_scraper/jobs.csv')
 
 descriptions = df['description'] # Get all description column cells
 id_number = df['id'] # Get all ids from column cells
+location = df['location'] # Get all location from column cells
+date = df['date_posted'] # Get all posting dates from column cells
 
 # File defined for output
-output_file = "job_data.json"
+output_file = "job_data2.json"
 
 # Check for output file, create if doesn't exist
 if not os.path.exists(output_file):
@@ -83,10 +99,10 @@ for i in range(len(descriptions)):
 
     # If current id already exists within the JSON file, then skip to the next id
     if id_number[i] in existing_keys:
-        print("skipped existing ID")
+        print(f"Skipped existing ID - {id_number[i]}")
         continue
 
-    results = analyze_description(descriptions[i], id_number[i])    
+    results = analyze_description(descriptions[i], id_number[i], location[i], date[i])    
 
     # Append results to the end of the JSON file
     data.extend(results)
@@ -95,5 +111,5 @@ for i in range(len(descriptions)):
     with open(output_file, "w") as file:
         json.dump(data, file, indent=4)
 
-    # 4.5 seconds between API call because of limit on tokens per minute
-    time.sleep(4.5)
+    # 5 seconds between API call because of limit on tokens per minute
+    time.sleep(7)
